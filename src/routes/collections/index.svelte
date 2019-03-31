179b9a82c77dbf7da086cdd8d9fa176a5208c0b4
Robert Hall <thebookofrobert@gmail.com>
2019-03-30 17:40:07 -0700

    Delete collections

---
+++ src/routes/collections/index.svelte
@@ -0,0 +1,49 @@
+<h1>Collections</h1>
+
+<!-- <h2>Node Collections</h2>
+<ul>
+	{#each collections.nodes as node}
+		<li><a href="/settings/admin/collections/{node.name}">{node.name}</a></li>
+	{/each}
+</ul>
+
+<h2>Edge Collections</h2>
+<ul>
+	{#each collections.edges as edge}
+		<li><a href="/settings/admin/collections/{edge.name}">{edge.name}</a></li>
+	{/each}
+</ul> -->
+
+<!-- <script context="module">
+	import { GET } from '../../_server/utils/loaders'
+	import { collections } from '../../stores/app-store'
+
+</script> -->
+<script>
+	
+	export let collections
+</script>
+
+<!-- <style type="text/scss">
+	ul {
+		list-style: none;
+		margin: 0 0 2rem;
+		padding: 0;
+		font: 1.6rem/1.2 $font;
+	}
+	li {
+		margin: 0 0 0.5rem;
+	}
+	a {
+		display: block;
+		padding: 1rem;
+		background-color: $gray-light;
+		border: 1px solid $gray-6;
+		border-radius: 3px;
+		text-decoration: none;
+		transition: background-color 0.15s ease-in-out;
+		&:hover {
+			background-color: $gray-7;
+		}
+	}
+</style> -->
