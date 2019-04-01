7019af56f9d56ccb00ea30aceb846a90a66eb02f
Robert Hall <thebookofrobert@gmail.com>
2019-04-01 09:39:14 -0700

    Better app setup

--- src/_server/services/app-setup.js
+++ src/_server/services/app-setup.js
@@ -7,9 +7,27 @@ export async function appSetup(app) {
 	// FIXME: restart app after running the following: ??????????
 
 	const Users = db.collection('users')
-	const exists = await Users.exists()
+	let exists = await Users.exists()
 	if (!exists) {
 		log(await Users.create(), 'Creating `Users` collection')
+	}
+
+	const Collections = db.collection('collections')
+	exists = await Collections.exists()
+	if (!exists) {
+		log(await Collections.create(), 'Creating `Collections` collection')
+	}
+	const News = db.collection('stallion-news')
+	exists = await News.exists()
+	if (!exists) {
+		log(await News.create(), 'Creating `News` collection')
+		log(await Collections.save({ name: 'news', description: 'Add news or posts to your site.' }))
+	}
+	const Tags = db.collection('stallion-tags')
+	exists = await Tags.exists()
+	if (!exists) {
+		log(await Tags.create(), 'Creating `Tags` collection')
+		log(await Collections.save({ name: 'tags', description: 'Tag items for your site.' }))
 	}
 
 	// check if app needs to be initialized / setup
